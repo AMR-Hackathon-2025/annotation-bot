7fb5bf0d026bc95e4ea1cd1ff58403006cf4fe8d
khalil-abudahab <khalil.abudahab@cgps.group>
2025-05-20 08:14:03 +0100

    Update query.mjs

--- query.mjs
+++ query.mjs
@@ -2,7 +2,7 @@ import { SqlDatabase } from "langchain/sql_db";
 import { DataSource } from "typeorm";
 import { ChatOpenAI } from "@langchain/openai";
 import { pull } from "langchain/hub";
-import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { ChatPromptTemplate, PromptTemplate } from "@langchain/core/prompts";
 import { z } from "zod";
 import { QuerySqlTool } from "langchain/tools/sql";
 
@@ -20,7 +20,8 @@ const db = await SqlDatabase.fromDataSourceParams({
 
 
 const llm = new ChatOpenAI({
-  model: "gpt-4o-mini",
+  model: "gpt-4.1",
+  // model: "gpt-4o",
   temperature: 0
 });
 
@@ -28,9 +29,27 @@ const queryPromptTemplate = await pull(
   "langchain-ai/sql-query-system-prompt"
 );
 
-// queryPromptTemplate.promptMessages.forEach((message) => {
-//   console.log(message.lc_kwargs.prompt.template);
+queryPromptTemplate.promptMessages[0].lc_kwargs.prompt.template = `
+Given an input question, create a syntactically correct {dialect} query to run to help find the answer. Unless the user specifies in his question a specific number of examples they wish to obtain, always limit your query to at most {top_k} results. You can order the results by a relevant column to return the most interesting examples in the database.
+
+When the input question refers to a protein, find all the COG IDs in bacteria relating to that protein and create a query against these COG IDs in cog_id column in psc table.
+
+Never query for all the columns from a specific table, only ask for a the few relevant columns given the question.
+
+Pay attention to use only the column names that you can see in the schema description. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
+
+Only use the following tables:
+{table_info}'
+`;
+
+// const queryPromptTemplate = new PromptTemplate({
+//   inputVariables: ["foo"],
+//   template: "Say {foo}",
 // });
+
+queryPromptTemplate.promptMessages.forEach((message) => {
+  console.log(message.lc_kwargs.prompt.template);
+});
 
 
 const queryOutput = z.object({
