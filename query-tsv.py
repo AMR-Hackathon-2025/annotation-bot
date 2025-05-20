a4cbd2310563d4a224cf740676dfe88fa937334d
khalil-abudahab <khalil.abudahab@cgps.group>
2025-05-20 11:19:32 +0100

    Create query-tsv.py

---
+++ query-tsv.py
@@ -0,0 +1,93 @@
+import getpass
+import os
+import sys
+from langchain_community.utilities import SQLDatabase
+from langchain.chat_models import init_chat_model
+from langchain_core.prompts import ChatPromptTemplate
+from typing_extensions import Annotated
+from typing_extensions import TypedDict
+from langchain_community.tools.sql_database.tool import QuerySQLDatabaseTool
+from langgraph.graph import START, StateGraph
+
+if not os.environ.get("POSTGRES_TSV_URL"):
+  os.environ["POSTGRES_TSV_URL"] = getpass.getpass("Enter Postgres DB URL: ")
+
+if not os.environ.get("OPENAI_API_KEY"):
+  os.environ["OPENAI_API_KEY"] = getpass.getpass("Enter API key for OpenAI: ")
+
+db = SQLDatabase.from_uri(os.environ.get("POSTGRES_TSV_URL"))
+
+class State(TypedDict):
+    question: str
+    query: str
+    result: str
+    answer: str
+
+llm = init_chat_model("gpt-4.1", model_provider="openai")
+
+system_message = """
+Given an input question, create a syntactically correct {dialect} query to run to help find the answer. Unless the user specifies in his question a specific number of examples they wish to obtain, always limit your query to at most {top_k} results. You can order the results by a relevant column to return the most interesting examples in the database.
+
+When the question can be related to COG IDs, query the related COG IDs against the dbxrefs column in annotations table instead of querying the product column.
+
+Never query for all the columns from a specific table, only ask for a the few relevant columns given the question.
+
+Pay attention to use only the column names that you can see in the schema description. Be careful to not query for columns that do not exist. Also, pay attention to which column is in which table.
+
+Only use the following tables:
+{table_info}'
+"""
+
+user_prompt = "Question: {input}"
+
+query_prompt_template = ChatPromptTemplate(
+    [("system", system_message), ("user", user_prompt)]
+)
+
+class QueryOutput(TypedDict):
+    """Generated SQL query."""
+
+    query: Annotated[str, ..., "Syntactically valid SQL query."]
+
+def write_query(state: State):
+    """Generate SQL query to fetch information."""
+    prompt = query_prompt_template.invoke(
+        {
+            "dialect": db.dialect,
+            "top_k": 10,
+            "table_info": db.get_table_info(),
+            "input": state["question"],
+        }
+    )
+    structured_llm = llm.with_structured_output(QueryOutput)
+    result = structured_llm.invoke(prompt)
+    return {"query": result["query"]}
+
+def execute_query(state: State):
+    """Execute SQL query."""
+    execute_query_tool = QuerySQLDatabaseTool(db=db)
+    return {"result": execute_query_tool.invoke(state["query"])}
+
+
+def generate_answer(state: State):
+    """Answer question using retrieved information as context."""
+    prompt = (
+        "Given the following user question, corresponding SQL query, "
+        "and SQL result, answer the user question.\n\n"
+        f'Question: {state["question"]}\n'
+        f'SQL Query: {state["query"]}\n'
+        f'SQL Result: {state["result"]}'
+    )
+    response = llm.invoke(prompt)
+    return {"answer": response.content}
+
+graph_builder = StateGraph(State).add_sequence(
+    [write_query, execute_query, generate_answer]
+)
+graph_builder.add_edge(START, "write_query")
+graph = graph_builder.compile()
+
+for step in graph.stream(
+    {"question": " ".join(sys.argv[1:])}, stream_mode="updates"
+):
+    print(step)
